205573115325e3dbce5a50dfd5d9f9d866f4b0c2
James Matsuba <james.matsuba@gmail.com>
2019-04-21 17:14:28 -0700

    temp remove auth

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -6,12 +6,12 @@ module Authentication
   end
 
   def authenticate
-    return unless Rails.application.secrets.authentication.present?
-
-    authenticate_with_http_basic do |username, password|
-      Rails.application.secrets.authentication['credentials'].any? do |credentials|
-        username == credentials['username'] && password == credentials['password']
-      end
-    end || request_http_basic_authentication
+    # return unless Rails.application.secrets.authentication.present?
+    #
+    # authenticate_with_http_basic do |username, password|
+    #   Rails.application.secrets.authentication['credentials'].any? do |credentials|
+    #     username == credentials['username'] && password == credentials['password']
+    #   end
+    # end || request_http_basic_authentication
   end
 end
