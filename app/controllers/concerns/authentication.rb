62e855922817918b6110f0bb27735308ab0016d2
James Matsuba <james.matsuba@gmail.com>
2019-04-24 21:58:52 -0700

    add back auth

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,16 +2,16 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
-    # before_action :authenticate
+    before_action :authenticate
   end
 
   def authenticate
-    # return unless Rails.application.secrets.authentication.present?
-    #
-    # authenticate_with_http_basic do |username, password|
-    #   Rails.application.secrets.authentication['credentials'].any? do |credentials|
-    #     username == credentials['username'] && password == credentials['password']
-    #   end
-    # end || request_http_basic_authentication
+    return unless Rails.application.secrets.authentication.present?
+
+    authenticate_with_http_basic do |username, password|
+      Rails.application.secrets.authentication['credentials'].any? do |credentials|
+        username == credentials['username'] && password == credentials['password']
+      end
+    end || request_http_basic_authentication
   end
 end
